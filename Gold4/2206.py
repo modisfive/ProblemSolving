72cc772cc7d37d868d6b264d0766d89ff5f9fc09
ng-lee <loginonlyyy@gmail.com>
2021-11-24 20:22:12 +0900

    Solved 15658.py

--- Gold4/2206.py
+++ Gold4/2206.py
@@ -1,18 +1,21 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+
+input = sys.stdin.readline
 
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
+
 
 def pArr(arr):
     for i in arr:
         print(i)
 
+
 def main():
     n, m = map(int, input().split())
     matrix = [list(map(int, input().strip())) for _ in range(n)]
-    visited = [[0]*m for _ in range(n)]
+    visited = [[9999] * m for _ in range(n)]
 
     def bfs():
         que = deque()
@@ -23,20 +26,29 @@ def main():
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if 0<=nx<m and 0<=ny<n and visited[ny][nx] == 0 and matrix[ny][nx] == 0:
+                if (
+                    0 <= nx < m
+                    and 0 <= ny < n
+                    and visited[y][x] + 1 < visited[ny][nx]
+                    and matrix[ny][nx] == 0
+                ):
                     visited[ny][nx] = visited[y][x] + 1
                     que.append((ny, nx, cnt))
-                if cnt == 1 and 0<=nx<m and 0<=ny<n and visited[ny][nx] == 0 and matrix[ny][nx] == 1:
+                if (
+                    cnt == 1
+                    and 0 <= nx < m
+                    and 0 <= ny < n
+                    and visited[y][x] + 1 < visited[ny][nx]
+                    and matrix[ny][nx] == 1
+                ):
                     visited[ny][nx] = visited[y][x] + 1
                     que.append((ny, nx, 0))
-            
-        
+
     bfs()
 
-    # if visited[n-1][m-1] == 0: print(-1)
-    # else: print(visited[n-1][m-1])
-
+    answer = visited[n - 1][m - 1] if visited[n - 1][m - 1] != 9999 else -1
+    print(answer)
     pArr(visited)
 
-if __name__ == "__main__":
-    main()
+
+main()
--- Silver2/15658_cp.py
+++ Silver2/15658_cp.py
@@ -10,6 +10,7 @@ results = []
 def calc(idx, acc, plus, minus, mul, div):
     if idx == length:
         results.append(acc)
+        return
 
     if plus > 0:
         calc(idx + 1, acc + numbers[idx], plus - 1, minus, mul, div)
@@ -18,7 +19,10 @@ def calc(idx, acc, plus, minus, mul, div):
     if mul > 0:
         calc(idx + 1, acc * numbers[idx], plus, minus, mul - 1, div)
     if div > 0:
-        calc(idx + 1, acc // numbers[idx], plus, minus, mul, div - 1)
+        if acc < 0:
+            calc(idx + 1, -(-acc // numbers[idx]), plus, minus, mul, div - 1)
+        else:
+            calc(idx + 1, acc // numbers[idx], plus, minus, mul, div - 1)
 
 
 def main():
@@ -26,6 +30,11 @@ def main():
     length = int(input())
     numbers = list(map(int, input().split()))
     plus, minus, mul, div = map(int, input().split())
+
+    calc(1, numbers[0], plus, minus, mul, div)
+
+    print(max(results))
+    print(min(results))
 
 
 main()
