c024641e073449de99400d7e99bb97fd148652ec
ng-lee <loginonlyyy@gmail.com>
2021-12-29 17:39:30 +0900

    Tried 1202.py

--- Gold2/1202.py
+++ Gold2/1202.py
@@ -8,8 +8,28 @@ def main():
     gems = [list(map(int, input().split())) for _ in range(n)]
     bags = [int(input()) for _ in range(k)]
 
-    print(gems)
-    print(bags)
+    occupied = [0] * (max(bags) + 1)
+    bags.sort()
+
+    def getBag(vol):
+        diff = 9999
+        bag = -1
+        for b in bags:
+            if vol <= b and abs(b - vol) < diff:
+                diff = abs(b - vol)
+                bag = b
+        return bag
+
+    for gem in gems:
+        vol = gem[0]
+        price = gem[1]
+        bag = getBag(vol)
+        if bag == -1:
+            continue
+        if (occupied[bag] == 0) or (occupied[bag] < price):
+            occupied[bag] = price
+
+    print(sum(occupied))
 
 
 main()
