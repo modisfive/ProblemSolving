6a860d8f7105f7965d3d96dfc0e2cbf586a57992
ng-lee <loginonlyyy@gmail.com>
2022-01-05 22:18:28 +0900

    Solved 2447.py

--- Silver1/2447.py
+++ Silver1/2447.py
@@ -2,13 +2,37 @@ import sys
 
 input = sys.stdin.readline
 
+STAR = "*"
+BLANK = " "
+
+
+def solve(arr, kind, y, x, size):
+    if kind == BLANK:
+        for i in range(y, y + size):
+            for j in range(x, x + size):
+                arr[i][j] = BLANK
+
+    else:
+        if size == 1:
+            arr[y][x] = STAR
+        else:
+            m = size // 3
+            for i in range(3):
+                for j in range(3):
+                    new_kind = STAR
+                    if i == 1 and j == 1:
+                        new_kind = BLANK
+                    solve(arr, new_kind, y + m * i, x + m * j, m)
+
 
 def main():
     n = int(input())
+    arr = [[BLANK] * n for _ in range(n)]
 
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            print("***\n* *\n***")
+    solve(arr, STAR, 0, 0, n)
+
+    for row in arr:
+        print("".join(row))
 
 
 main()
