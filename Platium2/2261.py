5379d82031479eec5c48248b363714a2aa64342f
ng-lee <loginonlyyy@gmail.com>
2022-01-08 00:38:53 +0900

    Solved 1654.py

---
+++ Platium2/2261.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def getD(p1, p2):
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+
+
+def main():
+    n = int(input())
+    array = [list(map(int, input().split())) for _ in range(n)]
+
+
+main()
