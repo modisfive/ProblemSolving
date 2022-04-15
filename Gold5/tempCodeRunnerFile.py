34d9399e1dea504dfd53ef2ac01019a498b11b50
ng-lee <loginonlyyy@gmail.com>
2022-04-16 00:44:56 +0900

    2636.py

---
+++ Gold5/tempCodeRunnerFile.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+
+    def check(num):
+        matrix = [int(x) for x in str(num)]
+        if len(matrix) == 1:
+            if num == 0:
+                return False
+            else:
+                return True
+        for i in range(len(matrix) - 1):
+            if matrix[i] > matrix[i + 1]:
+                continue
+            else:
+                return False
+        return True
+
+    answer = 0
+    cnt = 0
+    while True:
+        if check(answer):
+            cnt += 1
+        if cnt == n:
+            break
+        answer += 1
+
+    print(answer)
+
+
+main()
