75c9250abd290533a786c59193cdec10aece8a14
namgon <loginonlyyy@gmail.com>
2022-06-03 16:08:22 +0900

    13565.py

--- Silver2/13565.py
+++ Silver2/13565.py
@@ -1,37 +1,43 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+
+input = sys.stdin.readline
 
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
+
 
 def main():
     n, m = map(int, input().split())
     matrix = [list(map(int, input().strip())) for _ in range(n)]
 
-    def bfs(start):
+    def bfs(matrix, y, x):
         que = deque()
-        que.append(start)
+        que.append((y, x))
+        matrix[y][x] = -1
         while que:
             y, x = que.popleft()
-            matrix[y][x] = -1
+
+            if y == len(matrix) - 1:
+                return 1
+
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if 0<=nx<m and 0<=ny<n and matrix[ny][nx] == 0:
+                if 0 <= nx < m and 0 <= ny < n and matrix[ny][nx] == 0:
+                    matrix[ny][nx] = -1
                     que.append((ny, nx))
-    
+
+        return 0
+
     for i in range(m):
-        if matrix[0][i] == 0: bfs((0, i))
+        if matrix[0][i] == 0:
+            result = bfs(matrix, 0, i)
+            if result:
+                print("YES")
+                sys.exit(0)
 
-    answer = "NO"
-    for i in range(m):
-        if matrix[n-1][i] == -1: 
-            answer = "YES"
-            break
-
-    print(answer)
+    print("NO")
 
 
-if __name__ == "__main__":
-    main()
+main()
