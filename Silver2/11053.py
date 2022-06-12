7b08bf48bbb701c999e9739e5d4e72d193c9fd75
namgon <loginonlyyy@gmail.com>
2022-06-12 21:32:42 +0900

    11053.py

---
+++ Silver2/11053.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+    numbers = list(map(int, input().split()))
+
+    dp = [1] * n
+
+    for i in range(n):
+        for j in range(i):
+            if numbers[i] > numbers[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    print(max(dp))
+
+
+main()
