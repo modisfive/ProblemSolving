659abf19a217672ab6bb9b21b0057694ba8c8d9e
namgon <nglee.dev@gmail.com>
2022-10-17 21:51:08 +0900

    11000.py

--- Gold5/11000.py
+++ Gold5/11000.py
@@ -1,19 +1,20 @@
 import sys
+import heapq
 
 input = sys.stdin.readline
 
 
 n = int(input())
-classes = [list(map(int, input().split())) for _ in range(n)]
+times = [list(map(int, input().split())) for _ in range(n)]
 
-classes.sort(key=lambda x: (x[0], x[1] - x[0]))
+times.sort()
 
-time = 0
-answer = 0
+rooms = []
+heapq.heappush(rooms, times[0][1])
 
-for start, end in classes:
-    if time <= start:
-        answer += 1
+for i in range(1, n):
+    if rooms[0] <= times[i][0]:
+        heapq.heappop(rooms)
+    heapq.heappush(rooms, times[i][1])
 
-
-print(answer)
+print(len(rooms))
