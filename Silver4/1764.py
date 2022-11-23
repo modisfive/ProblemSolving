18ae7e412676a93c51d3a68f17d64d6eb69b5660
namgon <nglee.dev@gmail.com>
2022-11-23 14:51:00 +0900

    1764.py

--- Silver4/1764.py
+++ Silver4/1764.py
@@ -1,27 +1,29 @@
 import sys
+from collections import defaultdict
+
 input = sys.stdin.readline
 
-def main():
-    n, m = map(int, input().split())
-    
-    tmp = []
-    answer = []
 
-    for _ in range(n):
-        name = input().strip()
-        tmp.append(name)
-    
-    for _ in range(m):
-        name = input().strip()
-        if name in tmp:
-            answer.append(name)
+n, m = map(int, input().split())
+names = defaultdict(int)
 
-    answer.sort()
 
-    print(len(answer))
-    
-    for name in answer:
-        print(name, end='\n')
+for _ in range(n):
+    name = input().rstrip()
+    names[name] += 1
 
-if __name__ == "__main__":
-    main()
+for _ in range(m):
+    name = input().rstrip()
+    names[name] += 1
+
+answer = []
+
+for name, cnt in names.items():
+    if cnt == 2:
+        answer.append(name)
+
+answer.sort()
+
+print(len(answer))
+for name in answer:
+    print(name)
