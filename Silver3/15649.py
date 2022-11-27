14e50a853d9c7f4e8c5468951c3cc0af851dddc7
namgon <nglee.dev@gmail.com>
2022-11-28 01:19:31 +0900

    15649.py

--- Silver3/15649.py
+++ Silver3/15649.py
@@ -1,9 +1,22 @@
 import sys
-from itertools import permutations
 
 input = sys.stdin.readline
 
 
 n, m = map(int, input().split())
-for comb in list(permutations(range(1, n + 1), m)):
-    print(*comb)
+visited = [False] * (n + 1)
+
+
+def go(arr):
+    if len(arr) == m:
+        print(*arr)
+        return
+
+    for i in range(1, n + 1):
+        if not visited[i]:
+            visited[i] = True
+            go(arr + [i])
+            visited[i] = False
+
+
+go([])
