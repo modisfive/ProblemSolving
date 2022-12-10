649cecc7256b25feafafa761ae534490d01382a2
namgon <nglee.dev@gmail.com>
2022-12-10 16:10:10 +0900

    1937.py

--- Gold3/1937.py
+++ Gold3/1937.py
@@ -1,32 +1,34 @@
 import sys
-input = sys.stdin.readline
+
 sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
 
-def main():
-    n = int(input())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-    answer = 0
-    dp = [[0]*n for _ in range(n)]
+dx = (1, 0, -1, 0)
+dy = (0, 1, 0, -1)
 
-    def dfs(curr):
-        y, x = curr
-        if dp[y][x]: return dp[y][x]
-        dp[y][x] = 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<n and matrix[y][x] < matrix[ny][nx]:
-                dp[y][x] = max(dp[y][x], dfs((ny, nx))+1)
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+
+
+def dfs(y, x):
+    if dp[y][x] != 0:
         return dp[y][x]
 
-    for i in range(n):
-        for j in range(n):            
-            answer = max(answer, dfs((i, j)))
-        
-    print(answer)
+    dp[y][x] = 1
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= nx < n and 0 <= ny < n and matrix[y][x] < matrix[ny][nx]:
+            dp[y][x] = max(dp[y][x], dfs(ny, nx) + 1)
+    return dp[y][x]
 
-if __name__ == "__main__":
-    main()
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        answer = max(answer, dfs(i, j))
+
+print(answer)
