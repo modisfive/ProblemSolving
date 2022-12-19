638e5030237218ba83ac1add73a2c65ee0b63e3b
namgon <nglee.dev@gmail.com>
2022-12-19 16:44:10 +0900

    1213.py

--- Silver2/11724.py
+++ Silver2/11724.py
@@ -1,29 +1,29 @@
 import sys
 
-def main():
-    n, v = map(int, sys.stdin.readline().split())
-    matrix = [[0]*(n+1) for _ in range(n+1)]
-    for _ in range(v):
-        a, b = map(int, sys.stdin.readline().split())
-        matrix[a][b] = matrix[b][a] = 1
-    visited = [0]*(n+1)
+input = sys.stdin.readline
 
-    def dfs(start):
-        visited[start] = 1
-        for i in range(len(matrix[start])):
-            if matrix[start][i] == 1 and visited[i] == 0:
-                dfs(i)
-    
-    answer = 0
 
-    for i in range(1, n+1):
-        if visited[i] == 0:
-            answer += 1
+n, v = map(int, input().split())
+matrix = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(v):
+    a, b = map(int, input().split())
+    matrix[a][b] = 1
+    matrix[b][a] = 1
+visited = [0] * (n + 1)
+
+
+def dfs(node):
+    visited[node] = 1
+    for i in range(len(matrix[node])):
+        if matrix[node][i] == 1 and visited[i] == 0:
             dfs(i)
 
-    
-    print(answer)
 
+answer = 0
 
-if __name__ == "__main__":
-    main()
+for i in range(1, n + 1):
+    if visited[i] == 0:
+        answer += 1
+        dfs(i)
+
+print(answer)
