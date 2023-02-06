c463b5000255daafae59de22821a9df6635027fa
Lee, Nam Gon <loginonlyyy@gmail.com>
2023-02-06 15:00:05 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 148 ms, Memory: 14764 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,55 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	static void recur(int n, int m) {
-		int tmp = n;
-		if(n==m) {
-			for(int i=0;i<tmp;i++) {
-				System.out.print("____");
-			}
-			System.out.println("\"재귀함수가 뭔가요?\"");
-			for(int i=0;i<tmp;i++) {
-				System.out.print("____");
-			}
-			
-			System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			for(int i=0;i<tmp;i++) {
-				System.out.print("____");
-			}
-			System.out.println("라고 답변하였지.");
-			return;
-		}
-		
-		for(int i=0;i<tmp;i++) {
-			System.out.print("____");
-		}
-		System.out.println("\"재귀함수가 뭔가요?\"");
-		for(int i=0;i<tmp;i++) {
-			System.out.print("____");
-		}
-		System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		for(int i=0;i<tmp;i++) {
-			System.out.print("____");
-		}
-		System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		for(int i=0;i<tmp;i++) {
-			System.out.print("____");
-		}
-		System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-		recur(n+1,m);
-		
-		for(int i=0;i<tmp;i++) {
-			System.out.print("____");
-		}
-		System.out.println("라고 답변하였지.");
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		recur(0,n);
-		
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+
+		sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+		recursive(0, n);
+
+		System.out.println(sb);
 	}
 
+	private static void recursive(int i, int n) {
+		String blank = "";
+		for (int j = 0; j < i; j++) {
+			blank += "____";
+		}
+
+		sb.append(blank).append("\"재귀함수가 뭔가요?\"\n");
+
+		if (i == n) {
+			sb.append(blank).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+		} else {
+			sb.append(blank).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+			sb.append(blank).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+			sb.append(blank).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+			recursive(i + 1, n);
+		}
+		sb.append(blank).append("라고 답변하였지.\n");
+	}
 }
