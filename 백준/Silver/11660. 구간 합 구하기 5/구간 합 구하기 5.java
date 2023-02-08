68d2886e13931a91180f4fc44bcf1afb9ff09885
Lee, Nam Gon <loginonlyyy@gmail.com>
2023-02-08 16:25:54 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 840 ms, Memory: 118828 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,13 +18,8 @@ public class Main {
 		for (int i = 1; i < n + 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < n + 1; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-
-		for (int i = 1; i < n + 1; i++) {
-			for (int j = 1; j < n + 1; j++) {
-				board[i][j] += board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1];
+				int input = Integer.parseInt(st.nextToken());
+				board[i][j] = input + board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1];
 			}
 		}
 
