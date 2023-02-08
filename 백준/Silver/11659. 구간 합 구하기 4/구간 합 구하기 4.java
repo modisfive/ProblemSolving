ad8e76cb64ffc05c68ea3bad4abf16f36909eee7
Lee, Nam Gon <loginonlyyy@gmail.com>
2023-02-08 14:04:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 712 ms, Memory: 64748 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] numbers = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i < n; i++) {
+			numbers[i] += numbers[i - 1];
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			if (a == 0) {
+				sb.append(numbers[b]);
+			} else {
+				sb.append(numbers[b] - numbers[a - 1]);
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+}
