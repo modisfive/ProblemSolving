07262f9ff51a221a3da3c7e2eda2c376b654feb5
Lee, Nam Gon <loginonlyyy@gmail.com>
2023-02-20 14:11:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 720 ms, Memory: 72468 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,36 @@
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+
+def dfs(node):
+    global cnt
+
+    for nxt in graph[node]:
+        if visited[nxt] == 0:
+            visited[nxt] = cnt
+            cnt += 1
+            dfs(nxt)
+
+
+n, m, r = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n + 1):
+    graph[i].sort()
+
+visited = [0] * (n + 1)
+cnt = 1
+visited[r] = cnt
+cnt += 1
+dfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
