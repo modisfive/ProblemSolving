390b8f0481924a13e15a37cb22bdda8c5cd10070
Lee, Nam Gon <loginonlyyy@gmail.com>
2023-02-23 13:27:54 +0900

    [Gold IV] Title: 알파벳, Time: 836 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -5,47 +5,41 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int r, c;
-	static char[][] board;
-	static int answer;
-	static boolean[] visited;
-
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
+	static int R, C, res_cnt;
+	static int[] dx = { 0, 1, 0, -1 }; //우 하 좌 상
+	static int[] dy = { 1, 0, -1, 0 };//우 하 좌 상
+	static Character[][] map;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		board = new char[r][c];
-		for (int i = 0; i < r; i++) {
-			String row = br.readLine();
-			for (int j = 0; j < c; j++) {
-				board[i][j] = row.charAt(j);
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new Character[R][C];
+		long alpha_idx = 1 << 27;
+
+		for (int i = 0; i < R; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = str.charAt(j);
 			}
 		}
 
-		answer = Integer.MIN_VALUE;
-		visited = new boolean[26];
-		visited[board[0][0] - 'A'] = true;
-		dfs(0, 0, 1);
-
-		System.out.println(answer);
+		alpha_idx |= (1 << (map[0][0] - 64));
+		dfs(0, 0, alpha_idx, 1);
+		System.out.println(res_cnt);
 	}
 
-	private static void dfs(int y, int x, int cnt) {
-		answer = Math.max(answer, cnt);
+	private static void dfs(int y, int x, long alpha_idx, int cnt) {
 
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (0 <= ny && ny < r && 0 <= nx && nx < c && !visited[board[ny][nx] - 'A']) {
-				visited[board[ny][nx] - 'A'] = true;
-				dfs(ny, nx, cnt + 1);
-				visited[board[ny][nx] - 'A'] = false;
+		for (int idx = 0; idx < 4; idx++) {
+			int nx = x + dx[idx];
+			int ny = y + dy[idx];
+			if (0 <= nx && nx < R && 0 <= ny && ny < C && (alpha_idx & (1 << (map[nx][ny] - 64))) == 0) {
+				dfs(ny, nx, alpha_idx | 1 << (map[nx][ny] - 64), cnt + 1);
 			}
 		}
-	}
 
+		res_cnt = Math.max(cnt, res_cnt);
+	}
 }
