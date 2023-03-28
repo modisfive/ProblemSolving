32e7cc36b454961c707fe69f24148dd7339074cd
Lee, Nam Gon <nglee.dev@gmail.com>
2023-03-28 11:49:36 +0900

    [Bronze II] Title: 피보나치 수, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n + 1];
+		
+		if (n > 0) {
+			dp[1] = 1;
+		}
+		
+		if (n > 1) {
+			for (int i = 2; i < n + 1; i ++) {
+				dp[i] = dp[i - 1] + dp[i - 2];
+			}
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
+
+}
