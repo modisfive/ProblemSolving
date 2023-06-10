45faa23f14411901512737035bd82723a044b570
Lee, Nam Gon <nglee.dev@gmail.com>
2023-06-10 12:50:32 +0900

    [Gold V] Title: 인구 이동, Time: 776 ms, Memory: 118888 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -7,49 +7,47 @@ dx = (1, 0, -1, 0)
 dy = (0, 1, 0, -1)
 
 
-def main():
-    n, l, r = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-    answer = 0
+n, l, r = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+answer = 0
 
-    while True:
-        visited = [[0] * n for _ in range(n)]
-        que = deque()
-        flag = True
-        for i in range(n):
-            for j in range(n):
-                if visited[i][j] == 0:
-                    que.append((i, j))
-                    visited[i][j] = 1
-                    arr = [(i, j)]
-                    tmp = matrix[i][j]
-                    while que:
-                        y, x = que.popleft()
-                        for k in range(4):
-                            ny = y + dy[k]
-                            nx = x + dx[k]
-                            if (
-                                0 <= ny < n
-                                and 0 <= nx < n
-                                and visited[ny][nx] == 0
-                                and l <= abs(matrix[ny][nx] - matrix[y][x]) <= r
-                            ):
-                                visited[ny][nx] = 1
-                                que.append((ny, nx))
-                                arr.append((ny, nx))
-                                tmp += matrix[ny][nx]
-                    if len(arr) > 1:
-                        flag = False
-                        s = tmp // len(arr)
+while True:
+    visited = [[False] * n for _ in range(n)]
+    que = deque()
+    flag = True
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                que.append((i, j))
+                visited[i][j] = True
+                arr = [(i, j)]
+                s = matrix[i][j]
 
-                        for p in arr:
-                            y, x = p
-                            matrix[y][x] = s
-        if flag:
-            break
-        answer += 1
+                while que:
+                    y, x = que.popleft()
+                    for k in range(4):
+                        ny = y + dy[k]
+                        nx = x + dx[k]
+                        if (
+                            0 <= ny < n
+                            and 0 <= nx < n
+                            and not visited[ny][nx] 
+                            and l <= abs(matrix[ny][nx] - matrix[y][x]) <= r
+                        ):
+                            visited[ny][nx] = True
+                            que.append((ny, nx))
+                            arr.append((ny, nx))
+                            s += matrix[ny][nx]
 
-    print(answer)
+                if len(arr) > 1:
+                    flag = False
+                    new_pop = s // len(arr)
 
+                    for y, x in arr:
+                        matrix[y][x] = new_pop
+    if flag:
+        break
+    
+    answer += 1
 
-main()
+print(answer)
