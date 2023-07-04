ba8eb77fba73533247e689f3fa95e33d4b6e0188
Lee, Nam Gon <nglee.dev@gmail.com>
2023-07-04 22:10:48 +0900

    [Gold V] Title: 동전, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+
+tc = int(input())
+
+
+for _ in range(tc):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    target = int(input())
+
+    dp = [0] * (target + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(1, target + 1):
+            if 0 <= i - coin:
+                dp[i] += dp[i - coin]
+
+    print(dp[target])
