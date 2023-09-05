33a9fdf2685d2875579bfcfcd0eb7acad4e169c0
Lee, Nam Gon <nglee.dev@gmail.com>
2023-09-05 22:47:38 +0900

    [Gold II] Title: 사전, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/1256. 사전/사전.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n, m, k = map(int, input().split())
+dp = [[1] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+
+if dp[n][m] < k:
+    print(-1)
+    sys.exit()
+
+
+answer = ""
+while True:
+    if n == 0 or m == 0:
+        answer += "z" * m
+        answer += "a" * n
+        break
+
+    flag = dp[n - 1][m]
+    if k <= flag:
+        answer += "a"
+        n -= 1
+    else:
+        answer += "z"
+        m -= 1
+        k -= flag
+
+print(answer)
