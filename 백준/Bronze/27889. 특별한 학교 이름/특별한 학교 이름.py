b792b943fcf3e4b3245e4352f025e39077d47a28
Lee, Nam Gon <nglee.dev@gmail.com>
2023-09-21 01:09:13 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+store = {
+"NLCS": "North London Collegiate School",
+"BHA": "Branksome Hall Asia",
+"KIS": "Korea International School",
+"SJA": "St. Johnsbury Academy"
+}
+
+print(store[input().strip()])
