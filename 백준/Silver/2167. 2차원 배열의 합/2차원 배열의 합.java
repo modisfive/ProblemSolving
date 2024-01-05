63849ed8fdac49ddc6215ddfbc03b0f4b0b828e2
Lee, Nam Gon <nglee.dev@gmail.com>
2024-01-05 22:19:31 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1004 ms, Memory: 50116 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m, k;
+    static int[][] matrix;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        matrix = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            int d = Integer.parseInt(st.nextToken()) - 1;
+            int sum = 0;
+
+            for (int y = a; y < c + 1; y++) {
+                for (int x = b; x < d + 1; x++) {
+                    sum += matrix[y][x];
+                }
+            }
+
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
