c56f59467d58bbf95e4bcc298f1a4a1068c40fe5
Lee, Nam Gon <nglee.dev@gmail.com>
2024-02-24 04:50:33 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+N = int(input())
+
+if N == 0:
+    print(1)
+else:
+    answer = 1
+    for i in range(1, N + 1):
+        answer *= i
+    print(answer)
