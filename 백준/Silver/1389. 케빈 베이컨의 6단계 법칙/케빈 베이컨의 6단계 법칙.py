c293c0d80179047e95438b4812159c795bf97fc9
Lee, Nam Gon <nglee.dev@gmail.com>
2024-04-08 17:37:07 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 156 ms, Memory: 110792 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,44 +1,29 @@
 import sys
+
 input = sys.stdin.readline
-from collections import deque
 
-def main():
-    n, m = map(int, input().split())
-    matrix = [[0]*(n+1) for _ in range(n+1)]
+INF = float("inf")
 
-    for _ in range(m):
-        a, b = map(int, input().split())
-        matrix[a][b] = 1
-        matrix[b][a] = 1
 
-    answer = [0]*(n+1)
+n, m = map(int, input().split())
+dist = [[INF] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    dist[a][b] = 1
+    dist[b][a] = 1
 
-    def getNum(i, j):
-        que = deque()
-        que.append((i, 1))
+for i in range(1, n + 1):
+    dist[i][i] = 0
 
-        while que:
-            curr, cnt = que.popleft()
-            if matrix[curr][j] == 1: return cnt
-            else:
-                for k in range(1, n+1):
-                    if matrix[curr][k] == 1: que.append((k, cnt+1))
+for mid in range(1, n + 1):
+    for start in range(1, n + 1):
+        for end in range(1, n + 1):
+            dist[start][end] = min(dist[start][end], dist[start][mid] + dist[mid][end])
 
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            if i == j: continue
-            answer[i] += getNum(i, j)
+results = [0] * (n + 1)
+for start in range(1, n + 1):
+    for end in range(1, n + 1):
+        results[start] += dist[start][end]
 
-    min = 9999
-    idx = 0
-
-    for i in range(1, n+1):
-        
-        if answer[i]<min:
-            min = answer[i]
-            idx = i    
-    
-    print(idx)
-
-if __name__ == "__main__":
-    main()
+m = min(results[1:])
+print(results.index(m))
