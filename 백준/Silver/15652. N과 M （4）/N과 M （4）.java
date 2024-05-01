ee70faa93d5efdaf1bff118e070e4800e3780290
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-01 21:02:47 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 17072 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        selected = new int[m];
+
+        solve(0, 0);
+
+        System.out.println(sb);
+
+    }
+
+    private static void solve(int curr, int start) {
+        if (curr == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            selected[curr] = i + 1;
+            solve(curr + 1, i);
+        }
+    }
+}
