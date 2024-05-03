5753c092c5f6581301ea13fd2b1c8d816ae5e7b3
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-03 16:26:35 +0900

    [level 2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 10.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 16:11:23
+2024년 05월 03일 16:26:33
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,37 +1,34 @@
-from collections import deque
-
-INF = int(1e9)
+INF = float("inf")
 
 
 def solution(s):
+    totalLength = len(s)
+
     answer = INF
-    
-    for length in range(1, len(s) + 1):
-        que = deque(s.strip())
-        result = ""
-        curr = ""
-        cnt = 1
-        for _ in range(length):
-            curr += que.popleft()
-        
-        while que:
-            tmp = ""
-            for _ in range(length):
-                if que:
-                    tmp += que.popleft()
-            if curr == tmp:
-                cnt += 1
+
+    for length in range(1, totalLength + 1):
+        string = ""
+        prev = ""
+        count = 1
+
+        end = 0
+        for start in range(0, totalLength, length):
+            end = start + length
+            if prev == s[start:end]:
+                count += 1
             else:
-                if cnt == 1:
-                    result += curr
-                else:
-                    result += str(cnt) + curr
-                curr = tmp
-                cnt = 1
-        if cnt == 1:
-            result += curr
-        else:
-            result += str(cnt) + curr 
-        answer = min(answer, len(result))
-    
+                if count != 1:
+                    string += str(count)
+                string += prev
+
+                prev = s[start:end]
+                count = 1
+
+        if count != 1:
+            string += str(count)
+        string += prev
+        string += s[end:]
+
+        answer = min(answer, len(string))
+
     return answer
