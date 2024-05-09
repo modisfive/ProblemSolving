cfda54f19e36456319af549851fbef905d579df9
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-09 21:35:16 +0900

    [Gold I] Title: 비숍, Time: 660 ms, Memory: 123916 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -3,49 +3,47 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(graph, idx, cnt):
-    if idx == len(graph):
-        return cnt
+def solve(graph, curr, count):
+    if curr == len(graph):
+        return count
 
-    results = [0]
+    result = 0
+    result = max(result, solve(graph, curr + 1, count))
 
-    results.append(dfs(graph, idx + 1, cnt))
+    for y, x in graph[curr]:
+        if not daegakCheck1[y + x] and not daegakCheck2[n - 1 + x - y]:
+            daegakCheck1[y + x] = True
+            daegakCheck2[n - 1 + x - y] = True
 
-    for y, x in graph[idx]:
-        if board[y][x] == 1 and not diag1[y + x] and not diag2[x - y + n - 1]:
-            board[y][x] = 0
-            diag1[y + x] = True
-            diag2[x - y + n - 1] = True
+            result = max(result, solve(graph, curr + 1, count + 1))
 
-            results.append(dfs(graph, idx + 1, cnt + 1))
+            daegakCheck1[y + x] = False
+            daegakCheck2[n - 1 + x - y] = False
 
-            board[y][x] = 1
-            diag1[y + x] = False
-            diag2[x - y + n - 1] = False
-
-    return max(results)
+    return result
 
 
 n = int(input())
-chess = [[(i + j) % 2 == 0 for j in range(n)] for i in range(n)]
 board = [list(map(int, input().split())) for _ in range(n)]
 
 white = [[] for _ in range(2 * n - 1)]
 black = [[] for _ in range(2 * n - 1)]
 for i in range(n):
+    isWhite = i % 2 == 0
     for j in range(n):
         if board[i][j] == 1:
-            if chess[i][j]:
+            if isWhite:
                 white[i + j].append((i, j))
             else:
                 black[i + j].append((i, j))
+        isWhite = not isWhite
 
 
-diag1 = [False] * (2 * n - 1)
-diag2 = [False] * (2 * n - 1)
+daegakCheck1 = [False] * (2 * n - 1)
+daegakCheck2 = [False] * (2 * n - 1)
 
-white_cnt = dfs(white, 0, 0)
-black_cnt = dfs(black, 0, 0)
+whiteCount = solve(white, 0, 0)
+blackCount = solve(black, 0, 0)
 
 
-print(white_cnt + black_cnt)
+print(whiteCount + blackCount)
