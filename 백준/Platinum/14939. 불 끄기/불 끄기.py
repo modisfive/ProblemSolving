5399849a9581f7dcf1b8e92519903d2cd73a111f
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-10 16:13:27 +0900

    [Platinum IV] Title: 불 끄기, Time: 216 ms, Memory: 111724 KB -BaekjoonHub

--- 백준/Platinum/14939. 불 끄기/불 끄기.py
+++ 백준/Platinum/14939. 불 끄기/불 끄기.py
@@ -1,59 +1,68 @@
 import sys
 
 input = sys.stdin.readline
+
 INF = float("inf")
 
 dx = (1, 0, -1, 0, 0)
 dy = (0, 1, 0, -1, 0)
 
 
-def convert(s):
-    return s == "O"
+def turn(y, x):
+    for i in range(5):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if checkRange(ny, nx):
+            if board[ny][nx] == "O":
+                board[ny][nx] = "#"
+            elif board[ny][nx] == "#":
+                board[ny][nx] = "O"
 
 
-def check_range(y, x):
+def checkRange(y, x):
     return 0 <= y < 10 and 0 <= x < 10
 
 
-board = [list(map(convert, input().strip())) for _ in range(10)]
-answer = INF
-
-for first_row in range(1 << 10):
-    curr_board = []
-    for i in range(10):
-        curr_board.append(board[i][:])
-
+def turnButtom():
     count = 0
-
-    for col in range(10):
-        if first_row & (1 << col):
-            count += 1
-            for i in range(5):
-                nx = col + dx[i]
-                ny = dy[i]
-                if check_range(ny, nx):
-                    curr_board[ny][nx] = not curr_board[ny][nx]
+    targets = []
 
     for y in range(1, 10):
         for x in range(10):
-            if curr_board[y - 1][x]:
+            if board[y - 1][x] == "O":
                 count += 1
-                for i in range(5):
-                    ny = y + dy[i]
-                    nx = x + dx[i]
-                    if check_range(ny, nx):
-                        curr_board[ny][nx] = not curr_board[ny][nx]
+                targets.append((y, x))
+                turn(y, x)
 
-    flag = True
-    for col in range(10):
-        if curr_board[9][col]:
-            flag = False
+    for x in range(10):
+        if board[9][x] == "O":
+            count = INF
             break
 
-    if flag:
-        answer = min(answer, count)
+    for y, x in targets:
+        turn(y, x)
+
+    return count
 
 
+def solve(currX, prevCount):
+    if currX == 10:
+        return prevCount + turnButtom()
+
+    result = INF
+    result = min(result, solve(currX + 1, prevCount))
+
+    turn(0, currX)
+    result = min(result, solve(currX + 1, prevCount + 1))
+    turn(0, currX)
+
+    return result
+
+
+board = [list(input().strip()) for _ in range(10)]
+
+
+answer = solve(0, 0)
 if answer == INF:
     answer = -1
 
