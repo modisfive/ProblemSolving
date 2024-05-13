be7faea347b429e0ead820a06210a31cfbab8096
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-14 08:42:42 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,35 +1,27 @@
 import sys
 
-def main():
-    day = int(sys.stdin.readline())
-    matrix = [[]]
-    for i in range(day):
-        a, b = map(int, sys.stdin.readline().split())
-        matrix.append([a, b])
+input = sys.stdin.readline
 
-    payment = []
 
-    def dfs(cur, total):        
-        pay = total + matrix[cur][1]    
-        stop = True
-        
-        for i in range(cur + matrix[cur][0], day+1):
-            if i + matrix[i][0] - 1 < day+1:
-                dfs(i, pay)
-                stop = False
-        
-        if stop == True:
-            payment.append(pay)
+def solve(start):
+    if start == n:
+        return 0
 
-    for i in range(1, day+1):
-        if i + matrix[i][0] - 1 < day+1:
-            dfs(i, 0)
+    if dp[start] != -1:
+        return dp[start]
 
-    if payment:                                     # 진짜 치사하다
-        print(max(payment))
-    else:
-        print(0)
+    result = 0
+    for nextDay in range(start, n):
+        t, p = consults[nextDay]
+        if nextDay + t - 1 < n:
+            result = max(result, p + solve(nextDay + t))
 
-if __name__ == "__main__":
-    main()
-        
+    dp[start] = result
+    return dp[start]
+
+
+n = int(input())
+consults = [list(map(int, input().split())) for _ in range(n)]
+dp = [-1] * n
+
+print(solve(0))
