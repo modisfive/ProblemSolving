412a05285421cc1a436ff32bc77323f363d28fd0
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-14 16:01:33 +0900

    [Gold III] Title: 사다리 조작, Time: 2332 ms, Memory: 114440 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,67 +1,70 @@
 import sys
+
 input = sys.stdin.readline
-import itertools
-
-def pArr(arr):
-    for i in arr:
-        print(i)
-
-def main():
-    n, m, h = map(int, input().split())
-
-    matrix = [[0]*(2*n+1) for _ in range(2*h+1)]
-    tmp = []
-
-    for i in range(1, 2*h):
-        for j in range(1, 2*n):
-            if i%2 and not j%2: tmp.append((i, j))    
-
-    for _ in range(m):
-        y, x = map(int, input().split())
-        matrix[2*y-1][2*x] = 1
-        tmp.remove((2*y-1, 2*x))
-        if (2*y-1, 2*x-2) in tmp: tmp.remove((2*y-1, 2*x-2))
-        if (2*y-1, 2*x+2) in tmp: tmp.remove((2*y-1, 2*x+2))
-    
-    candidates = []
-    for i in range(4):
-        candidates += list(itertools.combinations(tmp, i))
-
-    for j in range(2*n):
-        for i in range(2*h):
-            if j%2: matrix[i][j] = 1
-
-    def ladder(start):
-        dest = start
-        for i in range(2*h):
-            if matrix[i][dest+1] == 1: dest += 2
-            elif matrix[i][dest-1] == 1 : dest -= 2
-        return start == dest
-
-    def check():        
-        isTrue = True
-        for i in range(2*n+1):
-            if i%2 and not ladder(i):
-                isTrue = False
-                break
-        if isTrue: return True
-    
-    result = -1
-    for tmp in candidates:
-        for arr in tmp:
-            y, x = arr
-            matrix[y][x] = 1
-        
-        if check():
-            result = len(tmp)
-            break
-
-        for arr in tmp:
-            y, x = arr
-            matrix[y][x] = 0
-
-    print(result)
 
 
-if __name__ == "__main__":
-    main()
+INF = float("inf")
+
+
+def go(start):
+    x = start
+    for y in range(h):
+        if x < ladderColumn and ladder[y][x]:
+            x += 1
+        elif 0 < x and ladder[y][x - 1]:
+            x -= 1
+    return x == start
+
+
+def check():
+    for start in range(n):
+        if not go(start):
+            return False
+    return True
+
+
+def ladderCheck(y, x):
+    if ladder[y][x]:
+        return False
+
+    if x + 1 < ladderColumn and ladder[y][x + 1]:
+        return False
+
+    if 0 <= x - 1 and ladder[y][x - 1]:
+        return False
+
+    return True
+
+
+def solve(count, start):
+    if count == 0:
+        return check()
+
+    for curr in range(start, ladderColumn * h):
+        y = curr // ladderColumn
+        x = curr % ladderColumn
+
+        if ladderCheck(y, x):
+            ladder[y][x] = True
+            if solve(count - 1, curr + 1):
+                return True
+            ladder[y][x] = False
+
+    return False
+
+
+n, m, h = map(int, input().split())
+ladderColumn = n - 1
+ladder = [[False] * ladderColumn for _ in range(h)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    ladder[a - 1][b - 1] = True
+
+answer = -1
+for c in range(4):
+    if solve(c, 0):
+        answer = c
+        break
+
+print(answer)
