54f9a0c7f849a96dc8292ff03f117658a7931491
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-19 16:41:40 +0900

    [Gold V] Title: 정수를 끝까지 외치자, Time: 108 ms, Memory: 108080 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Gold/25419. 정수를 끝까지 외치자/정수를 끝까지 외치자.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solve(curr):
+    if n < curr:
+        return 0
+
+    if dp[curr] != -1:
+        return dp[curr]
+
+    dp[curr] = 1
+    for nextNumber in range(curr + 1, curr + k + 1):
+        if solve(nextNumber) == 1:
+            dp[curr] = 0
+            break
+
+    return dp[curr]
+
+
+n, k = map(int, input().split())
+blacklist = list(map(int, input().split()))
+dp = [-1] * (n + 1)
+for black in blacklist:
+    dp[black] = 0
+
+
+solve(0)
+
+answer = 0
+for i in range(1, k + 1):
+    if solve(i) == 1:
+        answer = 1
+        break
+
+print(answer)
