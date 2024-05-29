50ee64f0053f6e223984ce36775e47a06a405971
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-29 10:40:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 208 ms, Memory: 121736 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -3,34 +3,11 @@ import sys
 input = sys.stdin.readline
 
 
-def update(index, num):
-    idx = index
-    while idx <= n:
-        tree[idx] += num
-        idx += idx & -idx
-
-
-def acc_sum(index):
-    idx = index
-    result = 0
-    while 0 < idx:
-        result += tree[idx]
-        idx -= idx & -idx
-    return result
-
-
-def make_tree():
-    for i in range(1, n + 1):
-        update(i, arr[i])
-
-
 n, m = map(int, input().split())
-arr = [0] + list(map(int, input().split()))
-
-tree = [0] * (n + 1)
-
-make_tree()
+prefixSum = [0] + list(map(int, input().split()))
+for i in range(1, n + 1):
+    prefixSum[i] += prefixSum[i - 1]
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    print(acc_sum(b) - acc_sum(a - 1))
+    i, j = map(int, input().split())
+    print(prefixSum[j] - prefixSum[i - 1])
