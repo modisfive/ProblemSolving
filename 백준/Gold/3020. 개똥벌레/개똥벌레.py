6757f4f0b844305420e861a711288454e5e12bcd
Lee, Nam Gon <nglee.dev@gmail.com>
2024-05-30 16:08:52 +0900

    [Gold V] Title: 개똥벌레, Time: 196 ms, Memory: 118232 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -6,32 +6,30 @@ INF = float("inf")
 
 
 n, h = map(int, input().split())
-
-bottom = [0] * (h + 1)
-top = [0] * (h + 1)
-
+top = [0] * (h + 2)
+bottom = [0] * (h + 2)
 for i in range(n):
     _h = int(input())
     if i % 2 == 0:
         bottom[_h] += 1
     else:
-        top[_h] += 1
+        top[h + 1 - _h] += 1
 
+for i in range(1, h + 1):
+    j = h - i + 1
+    top[i] += top[i - 1]
+    bottom[j] += bottom[j + 1]
 
-for i in range(h, 1, -1):
-    bottom[i - 1] += bottom[i]
-    top[i - 1] += top[i]
 
 answer = INF
 count = 0
 
 for height in range(1, h + 1):
-    cnt = bottom[height] + top[h - height + 1]
-
-    if cnt < answer:
-        answer = cnt
+    currBreakCount = top[height] + bottom[height]
+    if currBreakCount < answer:
+        answer = currBreakCount
         count = 1
-    elif answer == cnt:
+    elif currBreakCount == answer:
         count += 1
 
 print(answer, count)
