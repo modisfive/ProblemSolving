eb176418764887cddbf84115d7ccc4326b5e5a4f
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-05 12:11:10 +0900

    [Silver IV] Title: 수들의 합 2, Time: 120 ms, Memory: 110612 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -4,21 +4,23 @@ input = sys.stdin.readline
 
 
 n, m = map(int, input().split())
-a = [0] + list(map(int, input().split()))
+numbers = [0] + list(map(int, input().split()))
+prefixSum = [0] * (n + 1)
 for i in range(1, n + 1):
-    a[i] += a[i - 1]
+    prefixSum[i] = prefixSum[i - 1] + numbers[i]
 
-left, right = 0, 1
 answer = 0
-while right < n + 1 and left < n + 1:
-    s = a[right] - a[left]
+start = 1
+end = 1
+while end < n + 1:
+    s = prefixSum[end] - prefixSum[start - 1]
     if s == m:
+        start += 1
+        end += 1
         answer += 1
-        right += 1
-        left += 1
     elif s < m:
-        right += 1
+        end += 1
     else:
-        left += 1
+        start += 1
 
 print(answer)
