f4ccc8ded446f7a210ee9458623d02e16d93c321
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-13 20:45:25 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 868 ms, Memory: 221232 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,53 +3,42 @@ import sys
 input = sys.stdin.readline
 
 
-def main():
-    n = int(input())
-    goal = list(map(int, input().split()))
-    m = int(input())
-    cards = list(map(int, input().split()))
-
-    check = [0] * m
-
-    def lower(num):
-        left = 0
-        right = n - 1
-        result = -1
-        while left <= right:
-            mid = (left + right) // 2
-            if num == goal[mid]:
-                result = mid
-                right = mid - 1
-            elif num < goal[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return result
-
-    def upper(num):
-        left = 0
-        right = n - 1
-        result = -1
-        while left <= right:
-            mid = (left + right) // 2
-            if num == goal[mid]:
-                result = mid
-                left = mid + 1
-            elif num < goal[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return result
-
-    goal.sort()
-
-    for idx in range(m):
-        low = lower(cards[idx])
-        upp = upper(cards[idx])
-        if low != -1:
-            check[idx] = upp - low + 1
-
-    print(*check)
+n = int(input())
+cards = sorted(list(map(int, input().split())))
+m = int(input())
+targets = list(map(int, input().split()))
 
 
-main()
+def lowerBound(target):
+    left = 0
+    right = n
+
+    while left < right:
+        mid = (left + right) // 2
+        if cards[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
+
+
+def uppperBound(target):
+    left = 0
+    right = n
+
+    while left < right:
+        mid = (left + right) // 2
+        if target < cards[mid]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return left
+
+
+for target in targets:
+    lower = lowerBound(target)
+    upper = uppperBound(target)
+
+    print(upper - lower, end=" ")
