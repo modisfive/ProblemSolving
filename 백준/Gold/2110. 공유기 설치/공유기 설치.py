20f0fb5eae57942789b26d4a64d765a83bc4143b
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-14 19:56:01 +0900

    [Gold IV] Title: 공유기 설치, Time: 156 ms, Memory: 113532 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -3,24 +3,29 @@ import sys
 input = sys.stdin.readline
 
 
+def count(distance):
+    result = 1
+    start = houses[0]
+    for i in range(1, n):
+        if start + distance <= houses[i]:
+            result += 1
+            start = houses[i]
+    return result
+
+
 n, c = map(int, input().split())
 houses = sorted([int(input()) for _ in range(n)])
 
-start = 1
-end = houses[-1] - houses[0]
+left = 1
+right = houses[-1]
+answer = 0
 
-while start <= end:
-    mid = (start + end) // 2
-    house = houses[0]
-    cnt = 1
-    for i in range(1, n):
-        if house + mid <= houses[i]:
-            cnt += 1
-            house = houses[i]
-
-    if cnt >= c:
-        start = mid + 1
+while left <= right:
+    mid = (left + right) // 2
+    if c <= count(mid):
+        answer = mid
+        left = mid + 1
     else:
-        end = mid - 1
+        right = mid - 1
 
-print(start - 1)
+print(answer)
