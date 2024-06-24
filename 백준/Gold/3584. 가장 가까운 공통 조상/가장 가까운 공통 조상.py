cca9da51ecd955f338de969b4f2c4e0325e67967
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-24 18:11:50 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 128 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def getParents(start):
+    result = []
+    while start != -1:
+        result.append(start)
+        start = parents[start]
+    return result
+
+
+tc = int(input())
+for _ in range(tc):
+    n = int(input())
+    parents = [-1] * (n + 1)
+    for _ in range(n - 1):
+        a, b = map(int, input().split())
+        parents[b] = a
+
+    node1, node2 = map(int, input().split())
+    parents1 = getParents(node1)
+    parents2 = getParents(node2)
+
+    answer = -1
+    while parents1 and parents2:
+        p1 = parents1.pop()
+        p2 = parents2.pop()
+        if p1 == p2:
+            answer = p1
+        else:
+            break
+
+    print(answer)
