402c3dbc4be2f0d52948a82e239183f50204f0fb
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-26 21:49:58 +0900

    [Gold IV] Title: 친구비, Time: 100 ms, Memory: 34904 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.py
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -1,60 +1,44 @@
 import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10**7)
 
 input = sys.stdin.readline
 
-INF = float("inf")
 
+def dfs(curr):
+    result = costs[curr]
+    for nextNode in adjList[curr]:
+        if not visited[nextNode]:
+            visited[nextNode] = True
+            result = min(result, dfs(nextNode))
 
-def find(a):
-    if parents[a] != a:
-        parents[a] = find(parents[a])
-
-    return parents[a]
-
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-
-    if a == b:
-        return True
-
-    if a < b:
-        parents[b] = a
-        roots[a].extend(roots[b])
-        roots[b].clear()
-    elif b < a:
-        parents[a] = b
-        roots[b].extend(roots[a])
-        roots[a].clear()
-
-    return False
-
-
-def get_min_pay(root):
-    _m = INF
-    for node in roots[root]:
-        _m = min(_m, pays[node])
-
-    return _m
+    return result
 
 
 n, m, k = map(int, input().split())
-pays = [0] + list(map(int, input().split()))
-
-parents = list(range(n + 1))
-roots = [[i] for i in range(n + 1)]
-total = 0
-
+costs = [0] + list(map(int, input().split()))
+adjList = defaultdict(set)
 for _ in range(m):
-    a, b = map(int, input().split())
-    union(a, b)
+    v, w = map(int, input().split())
 
-for i in range(1, n + 1):
-    if len(roots[i]) != 0:
-        total += get_min_pay(i)
+    if v == w:
+        continue
 
-if total <= k:
-    print(total)
-else:
-    print("Oh no")
+    adjList[v].add(w)
+    adjList[w].add(v)
+
+for f in adjList:
+    adjList[f] = list(adjList[f])
+
+visited = [False] * (n + 1)
+answer = 0
+for start in range(1, n + 1):
+    if not visited[start]:
+        visited[start] = True
+        answer += dfs(start)
+
+if k < answer:
+    answer = "Oh no"
+
+print(answer)
