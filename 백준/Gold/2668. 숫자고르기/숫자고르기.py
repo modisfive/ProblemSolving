70ec4eba78c21d17e622e27bee73fe63d8e3af84
Lee, Nam Gon <nglee.dev@gmail.com>
2024-06-28 15:57:01 +0900

    [Gold V] Title: 숫자고르기, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -3,28 +3,25 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(curr, visitedIdx, visitedValue):
-    visitedIdx.add(curr)
-    nxt = numbers[curr]
-    visitedValue.add(nxt)
-    if numbers[curr] not in visitedIdx:
-        return dfs(nxt, visitedIdx, visitedValue)
-    else:
-        return (visitedIdx, visitedValue)
+def dfs(curr, start):
+    nextNode = array[curr]
+
+    if not visited[nextNode]:
+        visited[nextNode] = True
+        dfs(nextNode, start)
+    elif visited[nextNode] and nextNode == start:
+        result.append(start)
 
 
 n = int(input())
-numbers = [0] + [int(input()) for _ in range(n)]
+array = [0] + [int(input()) for _ in range(n)]
 
-results = set()
-for num in range(1, n + 1):
-    visitedIdx, visitedValue = dfs(num, set(), set())
-    if visitedIdx == visitedValue:
-        results = results.union(visitedIdx)
+result = []
+for start in range(1, n + 1):
+    visited = [False] * (n + 1)
+    visited[start] = True
+    dfs(start, start)
 
-results = list(results)
-results.sort()
-
-print(len(results))
-for res in results:
-    print(res)
+result.sort()
+print(len(result))
+print(*result, sep="\n")
