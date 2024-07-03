b5af9ee9c35d7f8ab823605f5f145a33db1c267c
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-03 18:21:24 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114228 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -7,24 +7,28 @@ dx = (1, 0, -1, 0)
 dy = (0, 1, 0, -1)
 
 
+def bfs():
+    que = deque()
+    que.append((0, 0))
+
+    visited = [[-1] * m for _ in range(n)]
+    visited[0][0] = 1
+
+    while que:
+        y, x = que.popleft()
+
+        if (y, x) == (n - 1, m - 1):
+            return visited[n - 1][m - 1]
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == -1 and board[ny][nx] == 1:
+                visited[ny][nx] = visited[y][x] + 1
+                que.append((ny, nx))
+
+
 n, m = map(int, input().split())
-matrix = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+board = [list(map(int, input().strip())) for _ in range(n)]
 
-
-que = deque()
-que.append((0, 0))
-dist = [[0] * m for _ in range(n)]
-dist[0][0] = 1
-
-while que:
-    y, x = que.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and matrix[ny][nx] == 1:
-            matrix[ny][nx] = 0
-            que.append((ny, nx))
-            dist[ny][nx] = dist[y][x] + 1
-            if nx == m - 1 and ny == n - 1:
-                print(dist[ny][nx])
-                sys.exit()
+print(bfs())
