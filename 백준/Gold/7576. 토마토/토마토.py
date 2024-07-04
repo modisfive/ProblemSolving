94c7a182a3e1d72a148ccf20ef9af251a2944294
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-04 16:51:09 +0900

    [Gold V] Title: 토마토, Time: 364 ms, Memory: 164708 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,44 +3,40 @@ from collections import deque
 
 input = sys.stdin.readline
 
-dx = (1, 0, -1, 0)
 dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
 
 
 m, n = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
+visited = [[-1] * m for _ in range(n)]
 que = deque()
 
-flag = True
+cnt = 0
 for i in range(n):
     for j in range(m):
-        if board[i][j] == 0:
-            flag = False
         if board[i][j] == 1:
+            visited[i][j] = 0
             que.append((i, j))
+        elif board[i][j] == -1:
+            cnt += 1
 
-if flag:
-    print(0)
-    sys.exit()
-
-answer = -1
+answer = 0
 while que:
-    length = len(que)
-    for _ in range(length):
-        y, x = que.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < m and board[ny][nx] == 0:
-                board[ny][nx] = 1
-                que.append((ny, nx))
-    answer += 1
+    y, x = que.popleft()
 
-for i in range(n):
-    for j in range(m):
-        if board[i][j] == 0:
-            print(-1)
-            sys.exit()
+    answer = max(answer, visited[y][x])
+    cnt += 1
+
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < n and 0 <= nx < m and board[ny][nx] == 0 and visited[ny][nx] == -1:
+            visited[ny][nx] = visited[y][x] + 1
+            que.append((ny, nx))
+
+if cnt != n * m:
+    answer = -1
 
 print(answer)
