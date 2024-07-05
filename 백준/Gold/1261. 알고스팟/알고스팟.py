d7f3bddd08cd7478cacd084b4c18b0b32c3f75d2
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-05 17:09:21 +0900

    [Gold IV] Title: 알고스팟, Time: 184 ms, Memory: 112096 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -3,37 +3,37 @@ import heapq
 
 input = sys.stdin.readline
 
-dx = (1, 0, -1, 0)
-dy = (0, 1, 0, -1)
-
 INF = float("inf")
+
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
+
+def bfs():
+    h = []
+    visited = [[INF] * m for _ in range(n)]
+
+    heapq.heappush(h, (0, 0, 0))
+    visited[0][0] = 0
+
+    while h:
+        d, y, x = heapq.heappop(h)
+
+        if visited[y][x] < d:
+            continue
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m and d + board[ny][nx] < visited[ny][nx]:
+                total = d + board[ny][nx]
+                visited[ny][nx] = total
+                heapq.heappush(h, (total, ny, nx))
+
+    return visited[n - 1][m - 1]
 
 
 m, n = map(int, input().split())
 board = [list(map(int, input().strip())) for _ in range(n)]
 
-dist = [[INF] * m for _ in range(n)]
-
-heap = []
-heapq.heappush(heap, (0, 0, 0))
-dist[0][0] = 0
-
-while heap:
-    d, y, x = heapq.heappop(heap)
-
-    if dist[y][x] < d:
-        continue
-
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if 0 <= ny < n and 0 <= nx < m:
-            if d + board[ny][nx] < dist[ny][nx]:
-                dist[ny][nx] = d + board[ny][nx]
-                heapq.heappush(heap, (dist[ny][nx], ny, nx))
-
-
-answer = dist[n - 1][m - 1]
-
-print(answer)
+print(bfs())
