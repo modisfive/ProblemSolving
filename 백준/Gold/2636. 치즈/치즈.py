a1d6915d2feaa8ba9a2bc2ef670209e1015e3917
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-09 14:56:23 +0900

    [Gold IV] Title: 치즈, Time: 140 ms, Memory: 114096 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -3,56 +3,49 @@ from collections import deque
 
 input = sys.stdin.readline
 
-dx = (1, 0, -1, 0)
 dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
 
 
-def pArr(arr):
-    for i in arr:
-        print(i)
+def bfs():
+    que = deque()
+    visited = [[False] * m for _ in range(n)]
+
+    que.append((0, 0))
+    visited[0][0] = True
+
+    cnt = 0
+
+    while que:
+        y, x = que.popleft()
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < m and not visited[ny][nx]:
+                visited[ny][nx] = True
+                if board[ny][nx] == 0:
+                    que.append((ny, nx))
+                elif board[ny][nx] == 1:
+                    board[ny][nx] = 0
+                    cnt += 1
+
+    return cnt
 
 
-def main():
-    n, m = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-    total_cnt = 0
-    for arr in matrix:
-        total_cnt += sum(arr)
+totalCnt = 0
+for i in range(n):
+    totalCnt += sum(board[i])
 
-    time = 0
+t = 0
+cnt = -1
+while totalCnt:
+    t += 1
+    cnt = bfs()
+    totalCnt -= cnt
 
-    while True:
-        time += 1
-
-        que = deque()
-        visited = [[0] * m for _ in range(n)]
-        que.append((0, 0))
-        visited[0][0] = 1
-        melt = []
-
-        while que:
-            y, x = que.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < m and 0 <= ny < n and not visited[ny][nx]:
-                    visited[ny][nx] = 1
-                    if matrix[ny][nx] == 1:
-                        melt.append((ny, nx))
-                    else:
-                        que.append((ny, nx))
-
-        if total_cnt - len(melt) == 0:
-            break
-        else:
-            total_cnt -= len(melt)
-
-        for y, x in melt:
-            matrix[y][x] = 0
-
-    print(time)
-    print(total_cnt)
-
-
-main()
+print(t)
+print(cnt)
