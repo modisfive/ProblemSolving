cf3a574829580da90837ee4c75c4136d9bccf415
Lee, Nam Gon <nglee.dev@gmail.com>
2024-07-11 00:35:37 +0900

    [Bronze I] Title: 소인수분해, Time: 108 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+
+n = int(input())
+
+for i in range(2, int(n**0.5) + 1):
+    while n % i == 0:
+        print(i)
+        n //= i
+
+if n != 1:
+    print(n)
