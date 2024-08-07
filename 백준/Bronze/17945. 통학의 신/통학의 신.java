1c9805e2e6e245739a4897d23c5f9ad5ebeeb263
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-07 13:47:40 +0900

    [Bronze III] Title: 통학의 신, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,42 +1,39 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int a, b;
+  static StringBuilder sb = new StringBuilder();
+  static int a, b;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-
-        int mid = -a;
-        int diff = 0;
-
-        while (true) {
-            int x1 = -a - diff;
-            int x2 = -a + diff;
-            if (f(x1) == 0 && f(x2) == 0) {
-                if (x1 == x2) {
-                    sb.append(x1);
-                } else {
-                    sb.append(x1).append(" ").append(x2);
-                }
-                break;
-            }
-            diff++;
-        }
-
-        System.out.println(sb);
-
+    for (int x = -1000; x < 1001; x++) {
+      if (x * x + 2 * a * x + b == 0) {
+        sb.append(x).append(" ");
+      }
     }
 
-    private static int f(int x) {
-        return x * x + 2 * a * x + b;
-    }
+    output();
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    a = Integer.parseInt(st.nextToken());
+    b = Integer.parseInt(st.nextToken());
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
