448f0a0d0a4898def6747618ee6193de355cf6d4
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-15 21:57:02 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+  static StringBuilder sb = new StringBuilder();
+  static int n;
+
+  public static void main(String[] args) throws IOException {
+    setUp();
+
+    int answer = 0;
+
+    for (int taek = 1; taek < n; taek++) {
+      for (int young = 1; young < n; young++) {
+        for (int nam = 1; nam < n; nam++) {
+
+          if (taek + young + nam != n) {
+            continue;
+          }
+          if (nam < young + 2) {
+            continue;
+          }
+          if (taek % 2 == 1) {
+            continue;
+          }
+
+          answer++;
+
+        }
+      }
+    }
+
+    sb.append(answer);
+
+    output();
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(br.readLine());
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+}
