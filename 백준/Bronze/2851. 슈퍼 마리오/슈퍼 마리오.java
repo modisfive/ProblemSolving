cf1bfec8e59dcc27a04d8b6e2b8aca562b60c3aa
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-15 22:19:04 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,28 +1,49 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder();
+  static int[] prefixSum;
 
-        int[] mushrooms = new int[10];
-        for (int i = 0; i < 10; i++) {
-            mushrooms[i] = Integer.parseInt(br.readLine());
-        }
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-        int point = 0;
-        for (int i = 0; i < 10; i++) {
-            if (point + mushrooms[i] >= 100) {
-                if (Math.abs(point + mushrooms[i] - 100) <= Math.abs(point - 100)) {
-                    point += mushrooms[i];
-                }
-                break;
-            }
-            point += mushrooms[i];
-        }
-
-        System.out.println(point);
+    int diff = Integer.MAX_VALUE;
+    int answer = -1;
+    for (int end = 0; end < 11; end++) {
+      int s = prefixSum[end];
+      if (Math.abs(100 - s) < diff) {
+        diff = Math.abs(100 - s);
+        answer = s;
+      } else if (Math.abs(100 - s) == diff && answer < s) {
+        answer = s;
+      }
     }
+
+    sb.append(answer);
+
+    output();
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    prefixSum = new int[11];
+    for (int i = 0; i < 10; i++) {
+      prefixSum[i + 1] = prefixSum[i] + Integer.parseInt(br.readLine());
+    }
+
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
