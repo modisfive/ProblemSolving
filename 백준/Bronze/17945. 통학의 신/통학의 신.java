4da4e6e30e490da167f5a9f9288b33c2c19478d1
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-15 21:32:44 +0900

    [Bronze III] Title: 통학의 신, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -13,20 +13,31 @@ public class Main {
   public static void main(String[] args) throws IOException {
     setUp();
 
-    for (int x = -1000; x < 1001; x++) {
-      if (x * x + 2 * a * x + b == 0) {
-        sb.append(x).append(" ");
+    int mid = -a;
+    if (f(mid) == 0) {
+      sb.append(mid);
+    } else {
+      int d = 1;
+      while (f(mid + d) != 0) {
+        d++;
       }
+      sb.append(mid - d).append(" ").append(mid + d);
     }
 
     output();
   }
 
+  private static int f(int x) {
+    return x * x + 2 * a * x + b;
+  }
+
   private static void setUp() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     a = Integer.parseInt(st.nextToken());
     b = Integer.parseInt(st.nextToken());
+
   }
 
   private static void output() throws IOException {
