10321b111a58dd166437afdd930421daac48a302
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-21 20:13:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 488 ms, Memory: 53756 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,42 +1,41 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static int[] prefixSum;
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    prefixSum = new int[n + 1];
 
-		int[] numbers = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
-		}
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i < n + 1; i++) {
+      prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
+    }
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+      sb.append(prefixSum[end] - prefixSum[start - 1]).append("\n");
+    }
+    output();
+  }
 
-		for (int i = 1; i < n; i++) {
-			numbers[i] += numbers[i - 1];
-		}
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			if (a == 0) {
-				sb.append(numbers[b]);
-			} else {
-				sb.append(numbers[b] - numbers[a - 1]);
-			}
-			sb.append("\n");
-		}
-
-		System.out.println(sb);
-
-	}
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
 }
