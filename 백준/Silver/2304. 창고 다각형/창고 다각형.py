02e32b857440adc92531c10d757bdb220ae2c705
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-22 10:51:10 +0900

    [Silver II] Title: 창고 다각형, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -4,21 +4,21 @@ input = sys.stdin.readline
 
 
 n = int(input())
-prefixSum = [0] * 1002
-suffixSum = [0] * 1002
+front = [0] * 1002
+back = [0] * 1002
 
 for _ in range(n):
     x, h = map(int, input().split())
-    prefixSum[x] = h
-    suffixSum[x] = h
+    front[x] = h
+    back[x] = h
 
-for i in range(1, 1002):
-    j = 1000 - i + 1
-    prefixSum[i] = max(prefixSum[i], prefixSum[i - 1])
-    suffixSum[j] = max(suffixSum[j], suffixSum[j + 1])
+for i in range(1, 1001):
+    front[i] = max(front[i], front[i - 1])
+    j = 1001 - i
+    back[j] = max(back[j], back[j + 1])
 
 answer = 0
-for i in range(1, 1001):
-    answer += min(prefixSum[i], suffixSum[i])
+for x in range(1, 1001):
+    answer += min(front[x], back[x])
 
 print(answer)
