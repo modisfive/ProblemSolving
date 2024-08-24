bb911a29dfcb3840e422350eef98ffd8b383794b
Lee, Nam Gon <nglee.dev@gmail.com>
2024-08-24 15:48:27 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 708 ms, Memory: 118012 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,41 +1,50 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static int[][] prefixSum;
 
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[][] board = new int[n + 1][n + 1];
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    prefixSum = new int[n + 1][n + 1];
+    for (int i = 1; i < n + 1; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 1; j < n + 1; j++) {
+        prefixSum[i][j] = prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+      }
+    }
 
-		for (int i = 1; i < n + 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j < n + 1; j++) {
-				int input = Integer.parseInt(st.nextToken());
-				board[i][j] = input + board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1];
-			}
-		}
+    for (int tc = 0; tc < m; tc++) {
+      st = new StringTokenizer(br.readLine());
+      int x1 = Integer.parseInt(st.nextToken());
+      int y1 = Integer.parseInt(st.nextToken());
+      int x2 = Integer.parseInt(st.nextToken());
+      int y2 = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
+      sb.append(
+          prefixSum[x2][y2] - prefixSum[x2][y1 - 1] - prefixSum[x1 - 1][y2] + prefixSum[x1 - 1][y1 - 1]
+      ).append("\n");
+    }
 
-			int answer = board[y2][x2] - board[y2][x1 - 1] - board[y1 - 1][x2] + board[y1 - 1][x1 - 1];
-			sb.append(answer).append("\n");
+    output();
+  }
 
-		}
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
-		System.out.println(sb);
-
-	}
 }
