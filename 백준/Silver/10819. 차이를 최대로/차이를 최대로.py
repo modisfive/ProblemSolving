7136776e3b3a39651948dde66b1bb2e0fc829095
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-11 20:16:40 +0900

    [Silver II] Title: 차이를 최대로, Time: 132 ms, Memory: 112052 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,21 +1,39 @@
 import sys
-import itertools
+
+input = sys.stdin.readline
+
+INF = float("inf")
 
 
-def main():
-    n = int(sys.stdin.readline())
-    matrix = list(map(int, sys.stdin.readline().split()))
-    total = list(itertools.permutations(matrix, n))
-    
-    answer = []
-    
-    for arr in total:
-        tmp = 0
-        for i in range(n-1):
-            tmp += abs(arr[i] - arr[i+1])
-        answer.append(tmp)
+def calc():
+    result = 0
+    for i in range(n - 1):
+        result += abs(selected[i] - selected[i + 1])
+    return result
 
-    print(max(answer))
-    
-if __name__ == "__main__":
-    main()
+
+def permutation(curr):
+    global answer
+    if curr == n:
+        answer = max(answer, calc())
+        return
+
+    for i in range(n):
+        if not is_selected[i]:
+            is_selected[i] = True
+            selected[curr] = numbers[i]
+            permutation(curr + 1)
+            is_selected[i] = False
+
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+selected = [0] * n
+is_selected = [False] * n
+
+answer = -INF
+
+permutation(0)
+
+print(answer)
