1983053c120c694cef7789143d953542becd930d
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-11 17:02:59 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,47 +1,59 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, s;
-    static int[] array;
-    static int answer;
+  static StringBuilder sb = new StringBuilder();
+  static int n, s, answer;
+  static int[] givens;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
+    answer = 0;
+    solve(0, 0);
 
-        array = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
-        }
-
-        answer = 0;
-        solve(0, 0);
-
-        if (s == 0) {
-            answer--;
-        }
-
-        System.out.println(answer);
-
+    if (s == 0) {
+      answer--;
     }
 
-    private static void solve(int curr, int prev) {
-        if (curr == n) {
-            if (prev == s) {
-                answer++;
-            }
-            return;
-        }
+    sb.append(answer);
+    output();
+  }
 
-        solve(curr + 1, prev + array[curr]);
-        solve(curr + 1, prev);
+  private static void solve(int curr, int prevSum) {
+    if (curr == n) {
+      if (prevSum == s) {
+        answer++;
+      }
+      return;
     }
+
+    solve(curr + 1, prevSum);
+    solve(curr + 1, prevSum + givens[curr]);
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    s = Integer.parseInt(st.nextToken());
+    givens = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      givens[i] = Integer.parseInt(st.nextToken());
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
