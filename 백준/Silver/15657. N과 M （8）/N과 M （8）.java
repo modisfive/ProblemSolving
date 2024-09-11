0ae13e779097c91b89fd27358c73b7066002785e
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-11 15:56:01 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,50 +1,60 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, m;
-    static int[] selected;
-    static int[] array;
-    static StringBuilder sb = new StringBuilder();
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static int[] selected;
+  static int[] givens;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+    Arrays.sort(givens);
+    solve(0, 0);
 
-        selected = new int[m];
-        array = new int[n];
+    output();
+  }
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(array);
-
-        solve(0, 0);
-
-        System.out.println(sb);
+  private static void solve(int curr, int start) {
+    if (curr == m) {
+      for (int num : selected) {
+        sb.append(num).append(" ");
+      }
+      sb.append("\n");
+      return;
     }
 
-    private static void solve(int curr, int start) {
-        if (curr == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(selected[i]).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = start; i < n; i++) {
-            selected[curr] = array[i];
-            solve(curr + 1, i);
-        }
+    for (int i = start; i < n; i++) {
+      selected[curr] = givens[i];
+      solve(curr + 1, i);
     }
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    selected = new int[m];
+    givens = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      givens[i] = Integer.parseInt(st.nextToken());
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
