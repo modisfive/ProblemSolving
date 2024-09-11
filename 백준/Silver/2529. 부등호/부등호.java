b1082c421e4c7db0dabf05c12f1ffcfaeab76ce6
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-11 16:57:34 +0900

    [Silver I] Title: 부등호, Time: 216 ms, Memory: 27068 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,69 +1,82 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int k;
-    static String[] inequalityList;
-    static boolean[] isSelected;
-    static long min = Long.MAX_VALUE;
-    static String minAnswer = "";
-    static long max = Long.MIN_VALUE;
-    static String maxAnswer = "";
-    static StringBuilder sb = new StringBuilder();
+  static StringBuilder sb = new StringBuilder();
+  static int n;
+  static char[] signs;
+  static int[] selected;
+  static boolean[] isSelected;
+  static List<String> results;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-        k = Integer.parseInt(br.readLine());
-
-        isSelected = new boolean[10];
-        inequalityList = new String[k];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < k; i++) {
-            inequalityList[i] = st.nextToken();
-        }
-
-        for (int i = 0; i < 10; i++) {
-            isSelected[i] = true;
-            solve(1, String.valueOf(i));
-            isSelected[i] = false;
-        }
-
-        System.out.println(maxAnswer);
-        System.out.println(minAnswer);
-
+    for (int i = 0; i < 10; i++) {
+      isSelected[i] = true;
+      selected[0] = i;
+      solve(0);
+      isSelected[i] = false;
     }
 
-    private static void solve(int curr, String prev) {
-        if (curr == k + 1) {
-            long number = Long.parseLong(prev);
-            if (max < number) {
-                max = number;
-                maxAnswer = prev;
-            }
-            if (number < min) {
-                min = number;
-                minAnswer = prev;
-            }
-            return;
-        }
+    sb.append(Collections.max(results)).append("\n").append(Collections.min(results));
 
-        for (int i = 0; i < 10; i++) {
-            if (isSelected[i]) {
-                continue;
-            }
+    output();
+  }
 
-            int lastNumber = Integer.parseInt(prev.substring(curr - 1));
-
-            if ((inequalityList[curr - 1].equals("<") && lastNumber < i)
-                    || (inequalityList[curr - 1].equals(">") && lastNumber > i)) {
-                isSelected[i] = true;
-                solve(curr + 1, prev + i);
-                isSelected[i] = false;
-            }
-        }
+  private static void solve(int curr) {
+    if (curr == n) {
+      String ret = "";
+      for (int num : selected) {
+        ret += num;
+      }
+      results.add(ret);
+      return;
     }
+
+    for (int i = 0; i < 10; i++) {
+      if (isSelected[i]) {
+        continue;
+      }
+
+      int prev = selected[curr];
+      if ((signs[curr] == '<' && prev < i) || (signs[curr] == '>' && prev > i)) {
+        isSelected[i] = true;
+        selected[curr + 1] = i;
+        solve(curr + 1);
+        isSelected[i] = false;
+      }
+    }
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    n = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    signs = new char[n];
+    for (int i = 0; i < n; i++) {
+      signs[i] = st.nextToken().charAt(0);
+    }
+    selected = new int[n + 1];
+    isSelected = new boolean[10];
+    results = new ArrayList<>();
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
