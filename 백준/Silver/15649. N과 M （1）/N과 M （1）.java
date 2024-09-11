71004aab1ece490f2e4e4c10b0c52ec4ad8355af
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-11 09:45:43 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22616 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,55 +1,58 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static StringBuilder sb;
-	static int n, m;
-	static int[] numbers;
-	static int[] result;
-	static boolean[] isSelected;
+  static StringBuilder sb = new StringBuilder();
+  static int n, m;
+  static int[] selected;
+  static boolean[] isSelected;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+    permutation(0);
 
-		numbers = new int[n];
-		result = new int[m];
-		isSelected = new boolean[n];
+    output();
+  }
 
-		for (int i = 0; i < n; i++) {
-			numbers[i] = i + 1;
-		}
+  private static void permutation(int curr) {
+    if (curr == m) {
+      for (int num : selected) {
+        sb.append(num).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
 
-		perm(0);
+    for (int i = 1; i < n + 1; i++) {
+      if (!isSelected[i]) {
+        isSelected[i] = true;
+        selected[curr] = i;
+        permutation(curr + 1);
+        isSelected[i] = false;
+      }
+    }
+  }
 
-		System.out.println(sb);
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+    selected = new int[m];
+    isSelected = new boolean[n + 1];
+  }
 
-	}
-
-	private static void perm(int currIdx) {
-		if (currIdx == m) {
-			for (int num : result) {
-				sb.append(num).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-
-		for (int i = 0; i < n; i++) {
-			if (!isSelected[i]) {
-				isSelected[i] = true;
-				result[currIdx] = numbers[i];
-				perm(currIdx + 1);
-				isSelected[i] = false;
-			}
-		}
-	}
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
 }
