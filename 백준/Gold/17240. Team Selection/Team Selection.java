78ba73397529af95646cbc1123833b9e40884f96
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-16 16:53:17 +0900

    [Gold I] Title: Team Selection, Time: 380 ms, Memory: 30300 KB -BaekjoonHub

--- 백준/Gold/17240. Team Selection/Team Selection.java
+++ 백준/Gold/17240. Team Selection/Team Selection.java
@@ -1,82 +1,100 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n, answer;
-    static int[][] members;
-    static Member[][] roles;
-    static boolean[] isRoleSelected, isMemberSelected;
+  static StringBuilder sb = new StringBuilder();
+  static int n, answer;
+  static int[][] stats;
+  static Player[][] players;
+  static boolean[] isRoleSelected;
+  static boolean[] isPlayerSelected;
 
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        n = Integer.parseInt(br.readLine());
-        members = new int[n][5];
-        roles = new Member[5][n];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                members[i][j] = Integer.parseInt(st.nextToken());
-                roles[j][i] = new Member(i, members[i][j]);
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            Arrays.sort(roles[i]);
-        }
-
-        answer = -1;
-        isRoleSelected = new boolean[5];
-        isMemberSelected = new boolean[n];
-
-        solve(0, 0);
-
-        System.out.println(answer);
-
+    for (int i = 0; i < 5; i++) {
+      Arrays.sort(players[i]);
     }
 
-    private static void solve(int curr, int prevPoint) {
-        if (curr == 5) {
-            answer = Math.max(answer, prevPoint);
-            return;
-        }
+    answer = Integer.MIN_VALUE;
+    solve(0, 0);
 
-        for (int i = 0; i < 5; i++) {
-            if (!isRoleSelected[i]) {
-                isRoleSelected[i] = true;
-                for (int j = 0; j < n; j++) {
-                    Member member = roles[i][j];
-                    if (!isMemberSelected[member.index]) {
-                        isMemberSelected[member.index] = true;
-                        solve(curr + 1, prevPoint + member.point);
-                        isMemberSelected[member.index] = false;
-                        break;
-                    }
-                }
-                isRoleSelected[i] = false;
-            }
-        }
+    sb.append(answer);
+    output();
+  }
+
+  private static void solve(int depth, int prevSum) {
+    if (depth == 5) {
+      answer = Math.max(answer, prevSum);
+      return;
     }
 
-    private static class Member implements Comparable<Member> {
+    for (int i = 0; i < 5; i++) {
+      if (isRoleSelected[i]) {
+        continue;
+      }
 
-        public int index;
-        public int point;
-
-        public Member(int index, int point) {
-            this.index = index;
-            this.point = point;
+      isRoleSelected[i] = true;
+      for (int j = 0; j < n; j++) {
+        Player player = players[i][j];
+        if (isPlayerSelected[player.index]) {
+          continue;
         }
 
-        @Override
-        public int compareTo(Member o) {
-            return o.point - this.point;
-        }
+        isPlayerSelected[player.index] = true;
+        solve(depth + 1, prevSum + player.stat);
+        isPlayerSelected[player.index] = false;
+        break;
+      }
+      isRoleSelected[i] = false;
     }
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    n = Integer.parseInt(br.readLine());
+    stats = new int[n][5];
+    players = new Player[5][n];
+    isRoleSelected = new boolean[5];
+    isPlayerSelected = new boolean[n];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < 5; j++) {
+        stats[i][j] = Integer.parseInt(st.nextToken());
+        players[j][i] = new Player(i, stats[i][j]);
+      }
+    }
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+  private static class Player implements Comparable<Player> {
+
+    int index;
+    int stat;
+
+    public Player(int index, int stat) {
+      this.index = index;
+      this.stat = stat;
+    }
+
+    @Override
+    public int compareTo(Player o) {
+      return o.stat - this.stat;
+    }
+  }
+
 }
