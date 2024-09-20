e46b0829a2637ef0064da0d5aa863e9a74f6888e
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-20 20:39:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 160 ms, Memory: 112936 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,34 +1,46 @@
 import sys
-from itertools import permutations
 
 input = sys.stdin.readline
 
+INF = float("inf")
+
+
+def calc(curr, op, prevResult):
+    if op == 0:
+        return prevResult + numbers[curr + 1]
+    elif op == 1:
+        return prevResult - numbers[curr + 1]
+    elif op == 2:
+        return prevResult * numbers[curr + 1]
+    else:
+        return int(prevResult / numbers[curr + 1])
+
+
+def permutations(curr, prevResult):
+    global min_answer, max_answer
+    if curr == n - 1:
+        max_answer = max(max_answer, prevResult)
+        min_answer = min(min_answer, prevResult)
+        return
+
+    for i in range(4):
+        if counts[i] == 0:
+            continue
+
+        counts[i] -= 1
+        permutations(curr + 1, calc(curr, i, prevResult))
+        counts[i] += 1
+
+
 n = int(input())
-nums = list(map(int, input().split()))
-tmp = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+counts = list(map(int, input().split()))
 
-ops = []
+max_answer = -INF
+min_answer = INF
 
-for idx, v in enumerate(tmp):
-    for _ in range(v):
-        ops.append(idx)
+permutations(0, numbers[0])
 
-results = []
 
-for orders in set(permutations(ops)):
-
-    result = nums[0]
-    for idx in range(len(orders)):
-        if orders[idx] == 0:
-            result += nums[idx + 1]
-        elif orders[idx] == 1:
-            result -= nums[idx + 1]
-        elif orders[idx] == 2:
-            result *= nums[idx + 1]
-        elif orders[idx] == 3:
-            result /= nums[idx + 1]
-            result = int(result)
-    results.append(result)
-
-print(max(results))
-print(min(results))
+print(max_answer)
+print(min_answer)
