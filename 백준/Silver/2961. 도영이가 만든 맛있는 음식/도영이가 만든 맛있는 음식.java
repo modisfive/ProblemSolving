959bc7dfe700fff8a41994c562e8c8ee3d2ce8bc
Lee, Nam Gon <modisfive899@gmail.com>
2024-09-20 22:35:11 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,50 +1,59 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n;
-	static int[][] gradients;
-	static int answer = Integer.MAX_VALUE;
+  static StringBuilder sb = new StringBuilder();
+  static int n, answer;
+  static int[][] gradients;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-		n = Integer.parseInt(br.readLine());
-		gradients = new int[n][2];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			gradients[i][0] = Integer.parseInt(st.nextToken());
-			gradients[i][1] = Integer.parseInt(st.nextToken());
-		}
+    answer = Integer.MAX_VALUE;
+    subset(0, 0, 1, 0);
 
-		subset(new ArrayList<>(), 0);
+    sb.append(answer);
+    output();
+  }
 
-		System.out.println(answer);
-	}
+  private static void subset(int curr, int count, int s, int b) {
+    if (curr == n) {
+      if (count == 0) {
+        return;
+      }
+      answer = Math.min(answer, Math.abs(s - b));
+      return;
+    }
 
-	private static void subset(List<int[]> list, int cnt) {
-		if (cnt == n) {
-			if (!list.isEmpty()) {
-				int t1 = 1; //신맛
-				int t2 = 0; //쓴맛
-				for (int[] gradient : list) {
-					t1 *= gradient[0];
-					t2 += gradient[1];
-				}
-				answer = Math.min(answer, Math.abs(t1 - t2));
-			}
-			return;
-		}
+    subset(curr + 1, count, s, b);
+    subset(curr + 1, count + 1, s * gradients[curr][0], b + gradients[curr][1]);
+  }
 
-		subset(list, cnt + 1);
-		list.add(gradients[cnt]);
-		subset(list, cnt + 1);
-		list.remove(gradients[cnt]);
-	}
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    n = Integer.parseInt(br.readLine());
+    gradients = new int[n][2];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      gradients[i][0] = Integer.parseInt(st.nextToken());
+      gradients[i][1] = Integer.parseInt(st.nextToken());
+    }
+
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
 }
