ee09c782aff02c1da2c1bb63cf75c132a0aaa047
Lee, Nam Gon <65810091+modisfive@users.noreply.github.com>
2024-09-21 21:56:30 +0900

    [Gold IV] Title: 스도쿠, Time: 628 ms, Memory: 116352 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -3,46 +3,51 @@ import sys
 input = sys.stdin.readline
 
 
-def get_cands(y, x):
-    flag = [True] * 10
-
-    for i in range(9):
-        flag[board[i][x]] = False
-
-    for i in range(9):
-        flag[board[y][i]] = False
-
-    sy = y // 3 * 3
-    sx = x // 3 * 3
-
-    for i in range(sy, sy + 3):
-        for j in range(sx, sx + 3):
-            flag[board[i][j]] = False
-
-    return [i for i in range(1, 10) if flag[i]]
-
-
-def dfs(idx):
-    if idx == len(empty):
+def dfs(curr):
+    if curr == len(zero_spots):
         for row in board:
             print(*row)
         sys.exit()
+        return
 
-    y, x = empty[idx]
-    cands = get_cands(y, x)
+    y, x = zero_spots[curr]
+    square_number = 3 * (y // 3) + x // 3
 
-    for cand in cands:
-        board[y][x] = cand
-        dfs(idx + 1)
-        board[y][x] = 0
+    for num in range(1, 10):
+        if (
+            not is_selected_by_col[x][num]
+            and not is_selected_by_row[y][num]
+            and not is_selected_by_square[square_number][num]
+        ):
+            is_selected_by_col[x][num] = True
+            is_selected_by_row[y][num] = True
+            is_selected_by_square[square_number][num] = True
+            board[y][x] = num
+            dfs(curr + 1)
+            is_selected_by_col[x][num] = False
+            is_selected_by_row[y][num] = False
+            is_selected_by_square[square_number][num] = False
 
 
 board = [list(map(int, input().split())) for _ in range(9)]
-empty = []
-for i in range(9):
-    for j in range(9):
-        if board[i][j] == 0:
-            empty.append((i, j))
+is_selected_by_row = [[False] * 10 for _ in range(9)]
+is_selected_by_col = [[False] * 10 for _ in range(9)]
+is_selected_by_square = [[False] * 10 for _ in range(9)]
+
+zero_spots = []
+
+for y in range(9):
+    for x in range(9):
+        if board[y][x] == 0:
+            zero_spots.append((y, x))
+            continue
+
+        number = board[y][x]
+        square_number = 3 * (y // 3) + x // 3
+
+        is_selected_by_row[y][number] = True
+        is_selected_by_col[x][number] = True
+        is_selected_by_square[square_number][number] = True
 
 
 dfs(0)
