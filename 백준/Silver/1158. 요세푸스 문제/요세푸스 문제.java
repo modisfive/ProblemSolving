a68965fa83688ff9264fbad31bb4f7656a424618
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-09 16:32:14 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 456 ms, Memory: 293696 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,39 +1,55 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		sb.append("<");
+  static StringBuilder sb = new StringBuilder();
+  static int n, k;
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+  public static void main(String[] args) throws IOException {
+    setUp();
 
-		boolean[] visited = new boolean[n];
-		int cnt = 0;
-		int check = 0;
-		int idx = 0;
-		while (cnt < n) {
-			if (!visited[idx]) {
-				check++;
-				if (check == k) {
-					visited[idx] = true;
-					sb.append(idx + 1).append(", ");
-					cnt += 1;
-					check = 0;
-				}
-			}
-			idx = (idx + 1) % n;
-		}
+    sb.append('<');
+    Deque<Integer> que = new ArrayDeque<>();
+    for (int i = 1; i < n + 1; i++) {
+      que.offer(i);
+    }
+    int count = 1;
+    while (!que.isEmpty()) {
+      int curr = que.pollFirst();
+      if (count == k) {
+        sb.append(curr).append(',').append(' ');
+        count = 1;
+      } else {
+        que.offer(curr);
+        count++;
+      }
+    }
 
-		sb.setLength(sb.length() - 2);
-		sb.append(">");
-		System.out.println(sb);
-	}
+    sb.deleteCharAt(sb.length() - 1);
+    sb.deleteCharAt(sb.length() - 1);
+    sb.append('>');
+    output();
+  }
+
+  private static void setUp() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
 
 }
