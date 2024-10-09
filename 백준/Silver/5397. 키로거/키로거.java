5f79195dd44f97243dac9e9a082a157b15cd6741
Lee, Nam Gon <modisfive899@gmail.com>
2024-10-09 15:18:53 +0900

    [Silver II] Title: 키로거, Time: 784 ms, Memory: 159260 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+  static StringBuilder sb = new StringBuilder();
+  static String target;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int testcases = Integer.parseInt(br.readLine());
+    for (int tc = 0; tc < testcases; tc++) {
+      target = br.readLine();
+      sb.append(solve()).append("\n");
+    }
+
+    output();
+  }
+
+  private static String solve() {
+    Deque<Character> left = new ArrayDeque<>();
+    Deque<Character> right = new ArrayDeque<>();
+
+    for (int i = 0; i < target.length(); i++) {
+      char curr = target.charAt(i);
+      if (curr == '<' && !left.isEmpty()) {
+        right.offer(left.pollLast());
+      } else if (curr == '>' && !right.isEmpty()) {
+        left.offer(right.pollLast());
+      } else if (curr == '-' && !left.isEmpty()) {
+        left.pollLast();
+      } else if (('a' <= curr && curr <= 'z') || ('A' <= curr && curr <= 'Z') || ('0' <= curr && curr <= '9')) {
+        left.offer(curr);
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!left.isEmpty()) {
+      result.append(left.pollFirst());
+    }
+    while (!right.isEmpty()) {
+      result.append(right.pollLast());
+    }
+
+    return result.toString();
+  }
+
+  private static void output() throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+  }
+
+}
